67f8524343c3774d64a219425ac18832eca55509
Dhiraj Uchil <dhirajuchil007@users.noreply.github.com>
2023-08-03 17:16:34 +0530

    initial

---
+++ main.py
@@ -0,0 +1,111 @@
+from fastapi import FastAPI, Depends, HTTPException, Header, status
+from sqlalchemy import create_engine, Column, String, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from sqlalchemy.exc import IntegrityError
+from pydantic import BaseModel
+from fastapi.security import OAuth2PasswordBearer 
+import bcrypt
+import jwt
+from jose import JWTError, jwt
+from typing import Any, Union, Optional
+from sqlalchemy.types import Time
+
+# Replace this secret key with a long, secure, and random string in your production environment
+SECRET_KEY = "thisisasecret"
+ALGORITHM = "HS256"
+
+# Replace 'DATABASE_URL' with your actual database connection string
+DATABASE_URL = "postgresql://postgres:abc123@localhost/queue_management"
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+#DB creation
+Base = declarative_base()
+#Tables 
+class Staff(Base):
+    __tablename__ = "staff"
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    username = Column(String, unique=True, index=True)
+    password = Column(String)
+    branch = Column(String)
+    contact = Column(String)
+    staff_type = Column(Integer)
+    lunch_time_start = Column(Time) 
+    lunch_duration_minutes = Column(Integer)
+    
+    
+
+Base.metadata.create_all(bind=engine)
+
+
+app = FastAPI()
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/token")
+
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+class StaffModel(BaseModel):
+    name: str
+    username: str
+    password: str
+    branch: str         
+    contact: str        
+    staff_type: int     
+    lunch_time_start: Optional[str] = None
+    lunch_duration_minutes: Optional[int] = 30
+
+def get_current_staff(token: str = Depends(oauth2_scheme)) -> Union[str, Any]:
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+    )
+    try:
+        payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
+        username: str = payload.get("sub")
+        if username is None:
+            raise credentials_exception
+        return {"username": username, "name": payload.get("name")}
+    except JWTError:
+        raise credentials_exception
+
+
+@app.post("/createStaff/")
+def create_staff(staff: StaffModel, db: Session = Depends(get_db)):
+    try:
+        hashed_password = bcrypt.hashpw(staff.password.encode("utf-8"), bcrypt.gensalt())
+        decoded_password = hashed_password.decode('utf-8')
+        db_staff = Staff(name=staff.name, username=staff.username, password=decoded_password,
+                         branch=staff.branch, contact=staff.contact, staff_type=staff.staff_type,
+                         lunch_time_start=staff.lunch_time_start, lunch_duration_minutes=staff.lunch_duration_minutes)
+        db.add(db_staff)
+        db.commit()
+        db.refresh(db_staff)
+        return "Success"
+    except IntegrityError:
+        raise HTTPException(status_code=400, detail="Username already exists")
+
+
+@app.post("/login/")
+def login_staff(username: str, password: str, db: Session = Depends(get_db)):
+    staff = db.query(Staff).filter(Staff.username == username).first()
+    if not staff or not bcrypt.checkpw(password.encode("utf-8"), staff.password.encode("utf-8")):
+        raise HTTPException(status_code=401, detail="Invalid credentials")
+    
+    token_payload = {"sub": staff.username, "name": staff.name}
+    token = jwt.encode(token_payload, SECRET_KEY, algorithm=ALGORITHM)
+
+    return {"name": staff.name, "token": token}
+
+@app.get("/getStaffDetails")
+def get_details(staff: dict = Depends(get_current_staff)):
+    return {"message": f"Welcome, {staff['name']} ({staff['username']})"}
